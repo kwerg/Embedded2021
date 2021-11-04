01915ddc7426954b86248335f2669a93974622bc
TaeHyeong Kim <0624kth@gmail.com$>
2021-11-04 15:13:08 +0900

    1.c 2.c upload

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myProject.h"
+
+int printMyName1(void) //1~2중 하나. 파일이름에 맞게 할 것
+{
+printf ("1. my name is 김태형\n"); //자기 이름
+return 1;
+}
+
+
---
+++ 2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myProject.h"
+
+int printMyName2(void) //1~2중 하나. 파일이름에 맞게 할 것
+{
+printf ("2. my name is 김태형\n"); //자기 이름
+return 1;
+}
